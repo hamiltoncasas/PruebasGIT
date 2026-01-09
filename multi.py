ae6ca1cb1ce8cc2b872c5c351f1491bc7a79f6f0
Hamilton Ricardo <42850040+hamiltoncasas@users.noreply.github.com>
2026-01-09 18:16:14 -0500

    Primer carga

---
+++ multi.py
@@ -0,0 +1,10 @@
+#print(5+5)
+def multiplicacion(num1, num2):
+    mul = num1 * num2
+    return mul
+
+numero1 = int(input("Ingrese primer número: "))
+numero2 = int(input("Ingrese segundo número: "))
+res = multiplicacion(numero1, numero2)
+print("El resultado es:", res)
+
